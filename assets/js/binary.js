e70b3f0a8391ce89dda87750804dc885a8371431
Phattisu <naphatism@hotmail.com>
2025-05-06 11:19:18 +0700

    3.0.0

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.56/---simulator","cdnUrl":"https://cdn.makecode.com","version":"2.2.0","target":"arcade","targetVersion":"2.0.56","repo":"phatisena/arcade-math-util"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v2.0.56/---simulator","cdnUrl":"https://cdn.makecode.com","version":"3.0.0","target":"arcade","targetVersion":"2.0.56","repo":"phatisena/arcade-math-util"}
 // total=1648371 new=28.72% cached=66.97% other=4.31%
 (function (ectx) {
 'use strict';
@@ -428,7 +428,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P247334(s) {
+function _main___P260272(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -830,13 +830,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P247334.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P247334.continuations = [  ]
+_main___P260272.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P260272.continuations = [  ]
 
-function _main___P247334_mk(s) {
+function _main___P260272_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P247334, depth: s.depth + 1,
+        parent: s, fn: _main___P260272, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42058,5 +42058,5 @@ const sprites_BaseSpriteSayRenderer__C226249_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P247334
+return _main___P260272
 })
