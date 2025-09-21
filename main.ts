e8234e589467d1119e16d85dd2379a537d3156f1
Phattikuna <naphatism@hotmail.com>
2025-09-21 10:00:22 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-
 namespace Math {
 
     export enum bitop {
@@ -25,33 +24,33 @@ namespace Math {
 
     function newArr(len: number) {
         const nularr: any[] = []
-        for (let _i = 0;_i < len;_i++) nularr.push(null)
+        for (let _i = 0; _i < len; _i++) nularr.push(null)
         return nularr
     }
 
     /**
-     * get modulo value with bit modulo power of two 'a%2^n = a&(2^n - 1)'
+     * get bit modulo power of two value with 'a%2^b = a&(b-1)'
      * @param number value to devide
      * @param modules devided number value and get zero if mod value equal to zero
      * @returns modulus number value if mod number is not equal to zero
      */
-    //%blockid=math_mod_bit2
-    //%block="$a mod2 $b"
-    //%a.defl=1 b.defl=2
+    //%blockid=math_mod_realcalculate
+    //%block="$a mod 2^$b"
+    //%a.defl=1 b.defl=1
     //%group="modulus"
     //%weight=10
-    export function mod2(a: number, b: number) {
+    export function mod2n(a: number, b: number) {
         if (isNaN(a) || isNaN(b)) return NaN
-        return b > 0 ? (a & (b-1) + b) & (b-1) : 0
+        return b > 0 ? ((a & (b - 1)) + b) & (b - 1) : 0
     }
 
     /**
-     * get modulo value with modulo 'a%b = a&(b-1)'
+     * get modulo value with rem in rem '((a%b)+b)%b'
      * @param number value to devide
      * @param modules devided number value and get zero if mod value equal to zero
      * @returns modulus number value if mod number is not equal to zero
      */
-    //%blockid=math_mod_home
+    //%blockid=math_mod_original
     //%block="$a mod $b"
     //%a.defl=1 b.defl=2
     //%group="modulus"
@@ -99,7 +98,7 @@ namespace Math {
         if (narr.length <= 0) return NaN
         switch (sumt) {
             case 0: return narr.reduce((cur, val) => cur + val, 0)
-            case 1: return narr.reduce((cur, val, idx) => cur + val * (offset ? (idx < abs(offset) ? offset : abs(idx + 1 - abs(offset))) : idx + 1), offset ? offset : 0)
+            case 1: return narr.reduce((cur, val, idx) => cur + val * (offset ? (idx < Math.abs(offset) ? offset : Math.abs(idx + 1 - Math.abs(offset))) : idx + 1), offset ? offset : 0)
             case 2: return narr.reduce((cur, val) => cur + val, 0) / narr.length
         }
         return NaN
@@ -116,7 +115,7 @@ namespace Math {
         shellSort = 1,
     }
 
-    /**
+    /**is 
      * get sorted with sorting algorithm format
      * @param number array to sort
      * @param sorting algorithm format
@@ -136,20 +135,22 @@ namespace Math {
     }
 
     function shellSort(narr: number[]) {
-        let j: number, i: number, nnarr = narr.slice(), gap = floor(nnarr.length / 2)
+        let j: number, i: number, nnarr = narr.slice(), gap = Math.floor(nnarr.length / 2)
         while (gap > 0) {
-            for (i = gap; i < nnarr.length; i++) { const val = nnarr[i]
-                for (j = i; (j >= 0 && nnarr[j - gap] > val); j -= gap) [nnarr[j], nnarr[j - gap] ] = [nnarr[j - gap], nnarr[j]]
-                nnarr[j] = val }
-            gap = floor(gap / 2)
+            for (i = gap; i < nnarr.length; i++) {
+                const val = nnarr[i]
+                for (j = i; (j >= 0 && nnarr[j - gap] > val); j -= gap) { const tmp = nnarr[j]; nnarr[j] = nnarr[j - gap], nnarr[j - gap] = tmp; }
+                nnarr[j] = val
+            }
+            gap = Math.floor(gap / 2)
         }
         return nnarr
     }
 
     function heapSort(narr: number[]) {
         const sortedArray = narr.slice(), n = sortedArray.length;
-        for (let i = Math.floor(n / 2) - 1; i >= 0; i--) heapify(sortedArray, n, i);// make heapify for every parent node
-        for (let i = n - 1; i >= 1; i--) [sortedArray[i], sortedArray[0]] = [sortedArray[0], sortedArray[i]], heapify(sortedArray, i, 0);// spliting every argument of heap
+        for (let i = Math.floor(n / 2) - 1; i >= 0; i--) heapify(sortedArray, n, i);
+        for (let i = n - 1; i >= 1; i--) { const tmp = sortedArray[i]; sortedArray[i] = sortedArray[0], sortedArray[0] = tmp; heapify(sortedArray, i, 0); }
         return sortedArray;
     }
 
@@ -160,10 +161,10 @@ namespace Math {
             const leftChild = 2 * currentIndex + 1, rightChild = 2 * currentIndex + 2;
             if (leftChild < n && arr[leftChild] > arr[largest]) largest = leftChild;
             if (rightChild < n && arr[rightChild] > arr[largest]) largest = rightChild;
-            if (largest !== currentIndex) [arr[currentIndex], arr[largest]] = [arr[largest], arr[currentIndex]], currentIndex = largest;
+            if (largest !== currentIndex) { const tmp = arr[currentIndex]; arr[currentIndex] = arr[largest], arr[largest] = tmp; }
             else break;
         }
-    }
+    }                                                                                                                
 
     /**
      * find maximum value in element of array number values
@@ -175,7 +176,7 @@ namespace Math {
     //%group="min and max"
     //%weight=10
     export function maxArr(narr: number[]) {
-        return narr.length > 0 ? narr.reduce((cur, val) => max(cur, val), narr[0]) : NaN
+        return narr.length > 0 ? narr.reduce((cur, val) => Math.max(cur, val), narr[0]) : NaN
     }
 
     /**
@@ -188,10 +189,10 @@ namespace Math {
     //%group="min and max"
     //%weight=5
     export function minArr(narr: number[]) {
-        return narr.length > 0 ? narr.reduce((cur, val) => min(cur, val), narr[0]) : NaN
+        return narr.length > 0 ? narr.reduce((cur, val) => Math.min(cur, val), narr[0]) : NaN
     }
 
-    const d2r = PI / 180
+    const d2r = Math.PI / 180
 
     /**
      * get convert number value from degree to radian
@@ -206,7 +207,7 @@ namespace Math {
         return nv * d2r
     }
 
-    const r2d = 180 / PI
+    const r2d = 180 / Math.PI
 
     /**
      * get convert number value from radian to degree
@@ -231,7 +232,7 @@ namespace Math {
     //%group="exponential"
     //%weight=15
     export function ln(x: number) {
-        return x > 0 ? Math.log(x) : NaN
+        return x > 0 ? x != 1 ? Math.log(x) : 0 : NaN
     }
 
     /**
@@ -245,7 +246,7 @@ namespace Math {
     //%group="exponential"
     //%weight=10
     export function logn(n: number, x: number) {
-        return n > 0 && x > 0 ? (Math.log(x) / Math.log(n)) : NaN
+        return n > 0 && x > 0 ? (Math.log(x) / Math.log(Math.abs(n))) : NaN
     }
 
     /**
@@ -258,7 +259,7 @@ namespace Math {
     //%group="exponential"
     //%weight=9
     export function log10(x: number) {
-        return x > 0 ? (log(x) / Math.LOG10E) : NaN
+        return x > 0 ? x != 1 ? (Math.log(x) / Math.log(10)) : 0 : NaN
     }
 
     /**
@@ -271,7 +272,7 @@ namespace Math {
     //%group="exponential"
     //%weight=8
     export function log2(x: number) {
-        return x > 0 ? (log(x) / Math.LOG2E) : NaN
+        return x > 0 ? x != 1 ? (Math.log(x) / Math.log(2)) : 0 : NaN
     }
 
     /**
@@ -284,7 +285,7 @@ namespace Math {
     //%group="exponential"
     //%weight=5
     export function expn(x: number) {
-        return x > 0 ? exp(x) : NaN
+        return x > 0 ? Math.exp(x) : NaN
     }
 
     /**
@@ -299,10 +300,7 @@ namespace Math {
     //%weight=20
     export function gcd(a: number, b: number): number {
         if (isNaN(a) || isNaN(b)) return NaN
-        while (b !== a) {
-            if (a > b) a -= b;
-            else       b -= a;
-        }
+        while (a !== b) { if (a > b) a -= b; else b -= a; }
         return a;
     }
 
@@ -318,7 +316,56 @@ namespace Math {
     //%weight=15
     export function lcm(a: number, b: number): number {
         if (isNaN(a) || isNaN(b)) return NaN
-        return a === 0 || b === 0 ? 0 : abs(a * b) / gcd(a, b); // Use Math.abs to ensure the LCM is positive
+        return a == 0 || b == 0 ? 0 : Math.abs(a * b) / gcd(a, b);
+    }
+
+    /**
+ * Calculate absolute with bit
+ * @param x number
+ * @returns bit abs value
+ */
+    //%blockid=math_gcd_bit
+    //%block="bit abs of $x"
+    //%group="number theory"
+    //%weight=13
+    export function babs(x: number) {
+        const mask = x >> 31;
+        return (x + mask) ^ mask;
+    }
+
+    /**
+     * Calculate the bit greatest common divisor (GCD) of two numbers by stein'sAlgorithm
+     * @param a number
+     * @param b number
+     * @returns gcd value
+     */
+    //%blockid=math_gcd_bit
+    //%block="bgcd of $a and $b"
+    //%group="number theory"
+    //%weight=12
+    export function bgcd(a: number, b: number): number {
+        if (a === 0) return b;
+        if (b === 0) return a;
+        let shift = 0;
+        while (((a | b) & 1) === 0) a >>= 1, b >>= 1, shift++;
+        while ((a & 1) === 0) a >>= 1;
+        while (b !== 0) { while ((b & 1) === 0) b >>= 1; if (a > b) { const temp = a; a = b, b = temp; } b = b - a; }
+        return a << shift;
+    }
+
+    /**
+     * Calculate the bit least common multiple (LCM) of two numbers with bit absolute and bit gcd
+     * @param a number
+     * @param b number
+     * @returns lcm value
+     */
+    //%blockid=math_lcm_bit
+    //%block="blcm of $a and $b"
+    //%group="number theory"
+    //%weight=11
+    export function blcm(a: number, b: number): number {
+        if (isNaN(a) || isNaN(b)) return NaN
+        return a === 0 || b === 0 ? 0 : babs(a * b) / bgcd(a, b);
     }
 
     /**
@@ -331,12 +378,11 @@ namespace Math {
     //%group="number theory"
     //%weight=10
     export function isPrime(n: number): boolean {
-        if (n <= 1) return false; // Numbers less than or equal to 1 are not prime
-        if (n <= 3) return true;  // 2 and 3 are prime
-        if ((n & 1) === 0 || (n & 2) === 0) return false; // Multiples of 2 and 3 are not prime
-        // Check for factors from 5 to sqrt(n)
-        for (let i = 5; i * i <= n; i += 6) if ((n & (i-1)) === 0 || (n & (i+1)) === 0) return false;
-        return true; // If no factors were found, n is prime
+        if (n <= 1) return false;         
+        if (n <= 3) return true;
+        if (n % 2 === 0 || n % 3 === 0) return false;
+        for (let i = 5; i * i <= n; i += 6) if (n % i === 0 || n % (i + 2) === 0) return false;
+        return true;
     }
 
     /**
@@ -381,9 +427,11 @@ namespace Math {
         buf.setNumber(NumberFormat.Int32LE, 0, i);
         let y = buf.getNumber(NumberFormat.Float32LE, 0);
         // n iteration Newton-Raphson
-        for (let j = 0 ; j < n ; j++) y = y * (1.5 - ((0.5 * x) * (y * y)));
+        while (n --> 0) y = y * (1.5 - ((0.5 * x) * (y * y)));
         return y;
     }
+
+
 
     /**
      * the Fast square root 'sqrt(x) = x*q_rsqrt(x)'
@@ -398,16 +446,16 @@ namespace Math {
     //%weight=2
     export function q_sqrt(x: number, n?: number) {
         if (!n || isNaN(n)) n = 1;
-        return x < 0 || isNaN(x) ? NaN : x * q_rsqrt(x, n)
+        return x <= 0 || isNaN(x) ? NaN : x * q_rsqrt(x, n)
     }
 
     const pal = "0123456789abcdefghijklmnopqrstuvwxyz"
 
     export function dec2base(nval: number, radix: number, digit?: number) {
         const neg = nval < 0
-        nval = round(abs(nval)), radix = constrain(radix, 2, pal.length)
+        nval = Math.round(Math.abs(nval)), radix = Math.max(2, Math.min(radix, pal.length))
         let ntxt = ""
-        while ((!digit && nval > 0) || (digit && ntxt.length < digit && nval > 0)) ntxt = pal.charAt(nval % radix) + ntxt, nval = floor(nval / radix)
+        while ((!digit && nval > 0) || (digit && ntxt.length < digit && nval > 0)) ntxt = pal.charAt(nval % radix) + ntxt, nval = Math.floor(nval / radix)
         if (!digit || (digit && ntxt.length > digit)) return ntxt
         while (ntxt.length < digit) ntxt = pal.charAt(0) + ntxt
         return neg ? "-" + ntxt : ntxt
@@ -416,9 +464,10 @@ namespace Math {
     export function base2dec(ntxt: string, radix: number) {
         if (ntxt.split('').some(v => pal.indexOf(v) < 0)) return NaN
         const neg = ntxt.charAt(0) == "-"
-        ntxt = ntxt.toLowerCase().substr(neg ? 1 : 0, ntxt.length - (neg ? 1 : 0)), radix = constrain(radix, 2, pal.length)
-        const nval = ntxt.split('').map((v, i) => (max(0, pal.indexOf(v)) * radix ** (ntxt.length - i - 1))).reduce((cur, val) => (cur + val), 0)
+        ntxt = ntxt.toLowerCase().substr(neg ? 1 : 0, ntxt.length - (neg ? 1 : 0)), radix = Math.clamp(2, pal.length, radix)
+        const nval = ntxt.split('').map((v, i) => (Math.max(0, pal.indexOf(v)) * radix ** (ntxt.length - i - 1))).reduce((cur, val) => (cur + val), 0)
         return neg ? -nval : nval
     }
 
 }
+
